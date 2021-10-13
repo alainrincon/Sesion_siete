3818dbab08abd49a7bf62da4c01fa3aa8a3c0105
Alain <alainrincon@gmail.com>
2021-10-13 01:03:49 +0000

    Test. Primer Push

---
+++ test R cloud.R
@@ -0,0 +1,12 @@
+### Esta e suna prueba para hacer push y pull
+
+autos <- mtcars
+mean(mtcars$wt)
+
+library(gitcreds)
+gitcreds_set()
+
+install.packages("gitcreds")
+library(gitcreds)
+
+gitcreds_get()
